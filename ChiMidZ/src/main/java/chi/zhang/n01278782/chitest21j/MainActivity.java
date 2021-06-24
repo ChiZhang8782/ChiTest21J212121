4e73db1d0d6c4a046b2344d49bc83c6dcc4b14a6
ChiZhang8782 <greg_zhang@hotmail.com>
2021-06-24 18:05:03 -0400

    Chi Zhang, 1st commit, before implement any functionality

---
+++ ChiMidZ/src/main/java/chi/zhang/n01278782/chitest21j/MainActivity.java
@@ -0,0 +1,14 @@
+package chi.zhang.n01278782.chitest21j;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
